a3e129f25bd1bc2b62c776513c8430f9d227c956
Michael Vitz <vitz.michael@googlemail.com>
2010-04-18 10:39:04 +0000

    Moved example to JUnit test example.

--- mockito/src/test/java/de/mvitz/examples/mockito/GreeterTest.java
+++ mockito/src/test/java/de/mvitz/examples/mockito/GreeterTest.java
@@ -13,18 +13,19 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class GreeterTest {
 
+    private Greeter greeter;
+
     @Mock private Greeting greeting;
-    private Greeter jut;
 
     @Before
     public void setUp() throws Exception {
-        jut = new Greeter(greeting);
+        greeter = new Greeter(greeting);
     }
 
     @Test
     public void testGreeting() throws Exception {
         given(greeting.greet("Test")).willReturn("Hello Test!");
-        String result = jut.greet("Test");
+        String result = greeter.greet("Test");
         assertThat(result, is("Hello Test!"));
     }
 
